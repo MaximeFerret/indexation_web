37540ed3ba4d246b9353f17061330db8720e8048
MaximeFerret <maximeferret3@gmail.com>
2026-01-13 14:45:17 +0100

    Add loader JSONL function

---
+++ TP2/indexer/loader.py
@@ -0,0 +1,32 @@
+import json
+from typing import List, Dict
+
+def load_jsonl(file_path: str) -> List[Dict]:
+    """
+    Load a JSONL file and return a list of dictionaries.
+    
+    Parameters
+    ----------
+    file_path: str
+        The file path JSONL we want to load.
+    
+    Return
+    ------
+    List[Dict]
+        A document list of the given JSONL
+    """
+    documents = []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line_number, lin in enumerate(file, start=1):
+            line=line.strip()
+            if not line:
+                continue
+
+            try:
+                document = json.loads(line)
+                documents.append(document)
+            except json.JSONDecodeError as e:
+                print(f"Invalid JSON at line {line_number}: {e}")
+    
+    return documents
